598452c326ae0ea82806d8ceff39686a534acc4b
GeOdin <madeleine.vanwinkel@nelen-schuurmans.nl>
2017-01-30 10:45:33 +0100

    Used urllib.urlencode for adding payload to the url

--- utils/get_data.py
+++ utils/get_data.py
@@ -2,19 +2,20 @@
 """Module for getting the data from the Lizard API."""
 import json
 import urllib2
+import urllib
 
 from .constants import BASE_URL
 
 
 def get_data(asset_type, payload):
     """Function to get the JSON from the Lizard client."""
-    # Set the payload key
-    page_size = payload.keys()[0]
+    # Encode the payload
+    payload_encoded = urllib.urlencode(payload)
 
     # Set the url
-    url = "{}{}/?{}={}".format(
+    url = "{}{}/?{}".format(
         BASE_URL, asset_type,
-        page_size, payload[page_size])
+        payload_encoded)
 
     # # Get the JSON
     response = urllib2.urlopen(url)
