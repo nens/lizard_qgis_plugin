5f71edd47cd2530d470dfa32023188aa035104ee
GeOdin <madeleine.vanwinkel@nelen-schuurmans.nl>
2017-04-07 11:20:40 +0200

    Download all assets.

--- utils/get_data.py
+++ utils/get_data.py
@@ -7,67 +7,76 @@ import urllib
 
 from .constants import BASE_URL
 
+DOWNLOAD_LIMIT = 1000
+PAYLOAD_FORMAT = "json"
+TASK_STATUS_PENDING = "PENDING"
+TASK_STATUS_SUCCESS = "SUCCESS"
+TASK_STATUS_FAILURE = "FAILURE"
 
-def get_data(asset_type, payload):
-    """Function to get the JSON from the Lizard client."""
-    # Encode the payload
-    payload_encoded = urllib.urlencode(payload)
 
-    # Set the url
-    url = "{}{}/?{}".format(BASE_URL, asset_type, payload_encoded)
-    print url
+def get_data(dockwidget, asset_type):
+    """Function to get the JSON with asset data from the Lizard client."""
+    # Download all the assets of an asset type
+    max_amount = get_max_amount(asset_type)
+    payload = {"async": "true", "format": PAYLOAD_FORMAT,
+               "page_size": DOWNLOAD_LIMIT, "page": 1}
+    current_amount = 0
+    results = []
 
-    # # Get the JSON
-    response = urllib2.urlopen(url)
-    r = json.load(response)
-    # print r  # task
-    payload2 = {"format": "json"}
-    payload_encoded2 = urllib.urlencode(payload2)
-    task_url = "{}?{}".format(r["url"], payload_encoded2)
-    # print task_url
-    # follow task_url
+    while current_amount < max_amount:
+        # Create a task to download the JSON async
+        payload_encoded = urllib.urlencode(payload)
+        task_start_url = "{}{}/?{}".format(
+            BASE_URL, asset_type, payload_encoded)
+        task_start_response = urllib2.urlopen(task_start_url)
+        task_start_data = json.load(task_start_response)
 
-    # # Get the JSON
-    response2 = urllib2.urlopen(task_url)
-    r2 = json.load(response2)
-    # print r2
-    # print r2["task_status"]  # PENDING
-    # print type(r2["task_status"])
+        # Get the task JSON
+        task_payload = {"format": "json"}
+        task_payload_encoded = urllib.urlencode(task_payload)
+        task_url = "{}?{}".format(task_start_data["url"], task_payload_encoded)
+        task_response = urllib2.urlopen(task_url)
+        task_data = json.load(task_response)
 
-    while str(r2["task_status"]) == "PENDING":
-        response2 = urllib2.urlopen(task_url)
-        r2 = json.load(response2)
-        # print r2
-        time.sleep(0.5)
-    if str(r2["task_status"]) == "SUCCESS":
-        # print 'ja'
-        # print r2  # {u'result_url': u'https://demo.lizard.net/media/downloads/da9d8f0e-1f95-4bd3-a52c-3a641d08f3e1/da9d8f0e-1f95-4bd3-a52c-3a641d08f3e1.json', u'task_id': u'da9d8f0e-1f95-4bd3-a52c-3a641d08f3e1', u'task_status': u'SUCCESS'}
-        result_url = r2["result_url"]
-        # print result_url  # https://demo.lizard.net/media/downloads/da9d8f0e-1f95-4bd3-a52c-3a641d08f3e1/da9d8f0e-1f95-4bd3-a52c-3a641d08f3e1.json
-        # print type(result_url)  # unicode
-        response3 = urllib2.urlopen(result_url)
-        r = json.load(response3)
-        # r = json.load(result_url.read())
-        # print r
-        # print type(r)
-        # break
-    elif str(r2["task_status"]) == "FAILURE":
-        print 'FAILUREe'
-    # print "m"
+        # Check whether the task is done
+        task_status = str(task_data["task_status"])
+        while task_status == TASK_STATUS_PENDING:
+            # Update the task JSON
+            task_response = urllib2.urlopen(task_url)
+            task_data = json.load(task_response)
+            task_status = str(task_data["task_status"])
+            # Loop every 0.5 seconds
+            time.sleep(0.5)
+        if task_status == TASK_STATUS_SUCCESS:
+            # Get the JSON with the data
+            result_url = task_data["result_url"]
+            result_response = urllib2.urlopen(result_url)
+            result_data = json.load(result_response)
+            for result in result_data["results"]:
+                results.append(result)
+        elif task_status == TASK_STATUS_FAILURE:
+            dockwidget.set_all_status_bars("Task failure.")
 
-    # while r2["task_status"] == "PENDING":
-    #     if r2["task_status"] == "SUCCES":
-    #         print "SUCCES!"
-    #         break
-    #         return
-    #     elif r2["task_status"] == "FAILURE":
-    #         print "FAILURE!"
-    #         break
-    #         return
-    #     time.sleep(1)
-
-    response3 = r["results"]
-    # count = r["count"]
+        # Get the results from the JSON
+        current_amount += DOWNLOAD_LIMIT
+        payload["page"] += 1
 
     # Return the results
-    return response3
+    return results
+
+
+def get_max_amount(asset_type):
+    """
+    Function to get the max amount of assets of an asset type.
+
+    In this function, the count property of max_amount_data is the maximum
+    amount of that asset type. A small request is done to the API to return
+    this amount.
+    """
+    payload = {"format": PAYLOAD_FORMAT, "page_size": 1}
+    payload_encoded = urllib.urlencode(payload)
+    max_amount_url = "{}{}/?{}".format(BASE_URL, asset_type, payload_encoded)
+    max_amount_response = urllib2.urlopen(max_amount_url)
+    max_amount_data = json.load(max_amount_response)
+    max_amount = max_amount_data["count"]
+    return max_amount
