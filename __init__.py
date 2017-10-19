646b548dbd75cab3c4d67d8b5b921e7cbfff6934
GeOdin <madeleine.vanwinkel@nelen-schuurmans.nl>
2017-10-19 11:31:34 +0200

    Also remove lizard-connector from init file. (#39)

--- __init__.py
+++ __init__.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 """ This script initializes the plugin, making it known to QGIS."""
-import os.path
-import sys
 
 
 # noinspection PyPep8Naming
@@ -11,7 +9,5 @@ def classFactory(iface):  # pylint: disable=invalid-name
     :param iface: A QGIS interface instance.
     :type iface: QgsInterface
     """
-    sys.path.append(os.path.join(os.path.dirname(__file__), "libs",
-                                 "lizard-connector"))
     from .lizard_viewer import LizardViewer
     return LizardViewer(iface)
