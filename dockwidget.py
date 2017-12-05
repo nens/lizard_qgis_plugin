8a970d94404a63a95e41e533979b2b573e7353df
GeOdin <madeleine.vanwinkel@gmail.com>
2017-12-05 15:58:08 +0100

    Remove redundant comments.

--- dockwidget.py
+++ dockwidget.py
@@ -4,9 +4,6 @@
 import os
 
 from PyQt4 import QtGui, uic
-# from PySide2 import QtUITools  # QUiLoader  # QtUITools
-# from PyQt4.uic import QUiLoader
-# from PyQt4.QtGui import QFile
 from PyQt4.QtCore import pyqtSignal
 from PyQt4.QtCore import QDateTime
 
@@ -17,8 +14,6 @@ from .utils.constants import STYLES_ROOT
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'dockwidget.ui'))
-# groupbox_ui = uic.loadUiType(os.path.join(
-#     os.path.dirname(__file__), 'groupbox_20172111.ui'))
 
 AREA_FILTER_CURRENT_VIEW = "Current view"
 TAB_PRIVATE_DATA = "Private data"
@@ -81,48 +76,6 @@ class LizardViewerDockWidget(QtGui.QDockWidget, FORM_CLASS):
                 raster_type = "{}{}".format(raster_type, UNSTYLED_POSTFIX)
             self.data_type_combobox_private.addItem(raster_type)
 
-#     def add_selection_for_data(self, data_type):
-#         """
-#         Add selection possibilities to the dockwidget for the selected data
-#         type.
-
-#         Args:
-#             (str) data_type: The type of data.
-#         """
-#         # Add groupbox wit the name of the data_type (uic?)
-#         self.groupbox = GroupBoxSelection()
-#         # self.groupbox.setupUi(groupbox, self)
-#         # loader = QUiLoader()  #
-#         # file = QFile("groupbox_20172111.ui")
-#         # file.open(QFile.ReadOnly)
-#         # groupbox = loader.load(file, None)
-#         # # myWidget = loader.load(file, None)
-#         # file.close()
-#         # # groupbox = QWidget()
-#         # # layout = QVBoxLayout(myWidget)
-#         # # groupbox.setGeometry
-#         # self.groupbox = groupbox
-#         # # # Load ui as part of dockwidget
-#         # # ## https://stackoverflow.com/questions/17012610/dock-widget-loaded
-#         # # ## -from-separate-ui-file-to-main-window
-#         # # # or
-#         # # # http://doc.qt.io/qt-4.8/designer-using-a-ui-file.html
-
-# # def load_ui(ui_file, parent=None):
-# #     loader = QUiLoader()
-# #     file = QFile(ui_file)
-# #     file.open(QFile.ReadOnly)
-# #     myWidget = loader.load(file, None)
-# #     file.close()
-# #     return myWidget
-# # main_window = load_ui("ui/main_window.ui")
-# # dock_widget = load_ui("ui/console.ui", main_window)
-# # main_window.show()
-# # main_window.addDockWidget(Qt.LeftDockWidgetArea, dock_widget)
-
-#         # Add area selection
-#         # If rain: add from, to and time interval
-
     def add_areafilters_to_combobox(self):
         """Add the area filters to the combobox."""
         for area_filter in AREA_FILTERS:
@@ -153,24 +106,3 @@ class LizardViewerDockWidget(QtGui.QDockWidget, FORM_CLASS):
         """Function to return all the status bars."""
         return [self.status_bar_private,
                 self.status_bar_public]
-
-
-# class GroupBoxSelection(QtGui.QGroupBox, groupbox_ui):
-#     """Module for creating the groupbox."""
-
-#     closingPlugin = pyqtSignal()
-
-#     def __init__(self, parent=None):
-#         """Constructor."""
-#         super(GroupBoxSelection, self).__init__(parent)
-#         # Set up the user interface from Designer.
-#         # After setupUI you can access any designer object by doing
-#         # self.<objectname>, and you can use autoconnect slots - see
-#         # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
-#         # #widgets-and-dialogs-with-auto-connect
-#         self.setupUi(self)
-
-#     # def closeEvent(self, event):
-#     #     """Module for closing the dockwidget."""
-#     #     self.closingPlugin.emit()
-#     #     event.accept()
